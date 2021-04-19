6c1d3178b09e9c9ec78cebb5b3d88e0e78892b16
Taylor Delver <taylor@webcitz.com>
2021-04-19 15:24:19 -0500

    add articles model with migration

---
+++ database/migrations/2021_04_19_200437_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('joomla_id');
+            $table->string('joomla_url');
+            $table->string('year')->nullable();
+            $table->string('make');
+            $table->string('model')->nullable();
+            $table->string('submodel')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
