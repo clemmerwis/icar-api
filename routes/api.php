f87426ba0252d0c2ff4b89c0014fe8594b1d61b8
Christopher Lemmer <lemmer.j.christopher@gmail.com>
2021-04-19 10:06:52 -0500

    Add Vehicle model and migration

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Vehicle;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/vehicles', function() {
+    return Vehicle::all();
+});
+
+Route::Post('/vehicles', function() {
+   return Vehicle::create([
+       'make' => 'Ford',
+       'model' => 'Focus',
+       'year' => '2006'
+   ]);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
